663925d6275dd389cae03c10543508282f3c82c5
slo_ow <icet25@naver.com>
2022-10-03 15:32:32 +0900

    update Japanese language & test class

--- src/school/report/GenerateGradeReport.java
+++ src/school/report/GenerateGradeReport.java
@@ -13,19 +13,19 @@ import java.util.ArrayList;
 
 public class GenerateGradeReport {
     School school = School.getInstance();
-    public static final String TITLE = " 수강생 학점 \t\t\n";
-    public static final String HEADER = " 이름  |  학번  |중점과목| 점수   \n";
+    public static final String TITLE = " 受講生 単位 \t\t\n";
+    public static final String HEADER = " 名前  |  学番  |重点科目| 点数   \n";
     public static final String LINE = "-------------------------------------\n";
     private StringBuffer buffer = new StringBuffer();
 
     public String getReport(){
-        ArrayList<Subject> subjectList = school.getSubjectList();  // 모든 과목에 대한 학점 산출
+        ArrayList<Subject> subjectList = school.getSubjectList();  // 全ての科目に対した単位の算出
         for( Subject subject : subjectList) {
             makeHeader(subject);
             makeBody(subject);
             makeFooter();
         }
-        return buffer.toString();  // String 으로 반환
+        return buffer.toString();  // String に返還
     }
 
     public void makeHeader(Subject subject){
@@ -38,7 +38,7 @@ public class GenerateGradeReport {
 
     public void makeBody(Subject subject){
 
-        ArrayList<Student> studentList = subject.getStudentList();  // 각 과목의 학생들
+        ArrayList<Student> studentList = subject.getStudentList();  // 各々の科目の学生達
 
         for(int i=0; i<studentList.size(); i++){
             Student student = studentList.get(i);
@@ -49,7 +49,7 @@ public class GenerateGradeReport {
             buffer.append(student.getMajorSubject().getSubjectName() + "\t");
             buffer.append(" | ");
 
-            getScoreGrade(student, subject.getSubjectId());  //학생별 해당과목 학점 계산
+            getScoreGrade(student, subject.getSubjectId());  //学生達の該当科目の単位の計算
             buffer.append("\n");
             buffer.append(LINE);
         }
@@ -60,17 +60,17 @@ public class GenerateGradeReport {
         ArrayList<Score> scoreList = student.getScoreList();
         int majorId = student.getMajorSubject().getSubjectId();
 
-        GradeEvaluation[] gradeEvaluation = {new BasicEvaluation(), new MajorEvaluation()};  //학점 평가 클래스들
+        GradeEvaluation[] gradeEvaluation = {new BasicEvaluation(), new MajorEvaluation()};  //単位評価クレス達
 
-        for(int i=0; i<scoreList.size(); i++){  // 학생이 가진 점수들
+        for(int i=0; i<scoreList.size(); i++){  // 学生が持ってる点数達
 
             Score score = scoreList.get(i);
-            if(score.getSubject().getSubjectId() == subjectId) {  // 현재 학점을 산출할 과목
+            if(score.getSubject().getSubjectId() == subjectId) {  //　現在の単位を算出する科目
                 String grade;
-                if(score.getSubject().getSubjectId() == majorId)  // 중점 과목인 경우
-                    grade = gradeEvaluation[Define.SAB_TYPE].getGrade(score.getPoint());  //중점 과목 학점 평가 방법
+                if(score.getSubject().getSubjectId() == majorId)  // 重点科目の場合
+                    grade = gradeEvaluation[Define.SAB_TYPE].getGrade(score.getPoint());  //重点科目の単位の評価方法
                 else
-                    grade = gradeEvaluation[Define.AB_TYPE].getGrade(score.getPoint()); // 중점 과목이 아닌 경우
+                    grade = gradeEvaluation[Define.AB_TYPE].getGrade(score.getPoint()); // 重点科目ではない場合
                 buffer.append(score.getPoint());
                 buffer.append(":");
                 buffer.append(grade);
